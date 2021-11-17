74c4bca058dc9c519c38e8e0ad927e8798960863
Anderson Costa <andersscosta2@gmail.com>
2021-11-17 10:18:26 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+let numero = Number(prompt("Insira um número"))
+let anterior = numero - 1
+let posterior = numero + 1
+alert('O número é: ' + numero + "\nO número anterior é: " + anterior + '\nO número posterior é: ' + posterior)
